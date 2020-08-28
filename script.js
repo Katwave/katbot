fa7cb45284d76e932d15e2db8550ead6ab539c48
Katwave <65369933+Katwave@users.noreply.github.com>
2020-08-28 03:45:24 +0200

    Updated Script,js file
    Added some functionality using more javascript Regular expressions

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 const form = document.querySelector("form");
 const input = document.getElementById("typingTextBox");
 const messageList = document.getElementById("messages");
@@ -41,219 +39,199 @@ nameOfConnectedUser.innerText = connectedUsername;
 console.log(connectedUsername)
 */
 
-chatContainers.addEventListener("click", () =>{
-   
-    messageField.style="display:flex";
-    numOfMessages.style="visibility:hidden";
-    mainIntro.style="display:none";
+chatContainers.addEventListener("click", () => {
+  messageField.style = "display:flex";
+  numOfMessages.style = "visibility:hidden";
+  mainIntro.style = "display:none";
 
-    const liBibot = document.createElement("li");
-    const defaultMessage = document.getElementById("defaultMessage");
+  const liBibot = document.createElement("li");
+  const defaultMessage = document.getElementById("defaultMessage");
 
-    liBibot.innerText = defaultMessage.innerText;
-        messageList.append(liBibot);
-        liBibot.setAttribute("class", "them");
+  liBibot.innerText = defaultMessage.innerText;
+  messageList.append(liBibot);
+  liBibot.setAttribute("class", "them");
 });
 
-backToHome.addEventListener("click", (e) =>{
+backToHome.addEventListener("click", (e) => {
+  e.preventDefault();
 
-    e.preventDefault();
-
-    messageField.style="display:none";
-    mainIntro.style="display:flex";
+  messageField.style = "display:none";
+  mainIntro.style = "display:flex";
 });
 
-navDotted.addEventListener("click", () =>{
-
-    menuContainer.style.display="flex";
-    mainIntro.style="display:none";
-    messageField.style="display:none";
+navDotted.addEventListener("click", () => {
+  menuContainer.style.display = "flex";
+  mainIntro.style = "display:none";
+  messageField.style = "display:none";
 });
 
-goHome.addEventListener("click", (e) =>{
+goHome.addEventListener("click", (e) => {
+  e.preventDefault();
 
-    e.preventDefault();
-
-    menuContainer.style.display="none";
-    mainIntro.style="display:flex";
+  menuContainer.style.display = "none";
+  mainIntro.style = "display:flex";
 });
 
-goToUsername.addEventListener("click", (e) =>{
+goToUsername.addEventListener("click", (e) => {
+  e.preventDefault();
 
-    e.preventDefault();
-
-    menuContainer.style.display="none";
-    messageField.style="display:none";
-    mainIntro.style="display:none";
-    usernameContainer.style="display:flex";
+  menuContainer.style.display = "none";
+  messageField.style = "display:none";
+  mainIntro.style = "display:none";
+  usernameContainer.style = "display:flex";
 });
 
-submitButton.addEventListener("click", (e) =>{
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
 
-    e.preventDefault();
-
-    nameOfConnectedUser.innerText = usernameTextBox.value;
-    mainIntro.style="display:flex";
-    menuContainer.style.display="none";
-    messageField.style="display:none";
-    usernameContainer.style="display:none"
-    profileUsername.innerText = usernameTextBox.value;
+  nameOfConnectedUser.innerText = usernameTextBox.value;
+  mainIntro.style = "display:flex";
+  menuContainer.style.display = "none";
+  messageField.style = "display:none";
+  usernameContainer.style = "display:none";
+  profileUsername.innerText = usernameTextBox.value;
 });
 
-fromAboutToHome.addEventListener("click", (e) =>{
+fromAboutToHome.addEventListener("click", (e) => {
+  e.preventDefault();
 
-    e.preventDefault();
-
-    mainIntro.style="display:flex";
-    menuContainer.style.display="none";
-    messageField.style="display:none";
-    usernameContainer.style="display:none";
-    aboutContainer.style="display:none";
+  mainIntro.style = "display:flex";
+  menuContainer.style.display = "none";
+  messageField.style = "display:none";
+  usernameContainer.style = "display:none";
+  aboutContainer.style = "display:none";
 });
 
-goToAbout.addEventListener("click", (e) =>{
+goToAbout.addEventListener("click", (e) => {
+  e.preventDefault();
 
-    e.preventDefault();
-
-    mainIntro.style="display:none";
-    menuContainer.style.display="none";
-    messageField.style="display:none";
-    usernameContainer.style="display:none";
-    aboutContainer.style="display:flex";
+  mainIntro.style = "display:none";
+  menuContainer.style.display = "none";
+  messageField.style = "display:none";
+  usernameContainer.style = "display:none";
+  aboutContainer.style = "display:flex";
 });
 
-fromContactToHome.addEventListener("click", (e) =>{
+fromContactToHome.addEventListener("click", (e) => {
+  e.preventDefault();
 
-    e.preventDefault();
-
-    mainIntro.style="display:flex";
-    menuContainer.style.display="none";
-    messageField.style="display:none";
-    usernameContainer.style="display:none";
-    contactContainer.style="display:none";
+  mainIntro.style = "display:flex";
+  menuContainer.style.display = "none";
+  messageField.style = "display:none";
+  usernameContainer.style = "display:none";
+  contactContainer.style = "display:none";
 });
 
-goToContact.addEventListener("click", (e) =>{
+goToContact.addEventListener("click", (e) => {
+  e.preventDefault();
 
-    e.preventDefault();
-
-    mainIntro.style="display:none";
-    menuContainer.style.display="none";
-    messageField.style="display:none";
-    usernameContainer.style="display:none";
-    contactContainer.style="display:flex";
+  mainIntro.style = "display:none";
+  menuContainer.style.display = "none";
+  messageField.style = "display:none";
+  usernameContainer.style = "display:none";
+  contactContainer.style = "display:flex";
 });
 
-fromProfileToHome.addEventListener("click", (e) =>{
+fromProfileToHome.addEventListener("click", (e) => {
+  e.preventDefault();
 
-    e.preventDefault();
-
-    mainIntro.style="display:flex";
-    menuContainer.style.display="none";
-    messageField.style="display:none";
-    usernameContainer.style="display:none";
-    profileContainer.style="display:none";
+  mainIntro.style = "display:flex";
+  menuContainer.style.display = "none";
+  messageField.style = "display:none";
+  usernameContainer.style = "display:none";
+  profileContainer.style = "display:none";
 });
 
-goToProfile.addEventListener("click", (e) =>{
+goToProfile.addEventListener("click", (e) => {
+  e.preventDefault();
 
-    e.preventDefault();
-
-    mainIntro.style="display:none";
-    menuContainer.style.display="none";
-    messageField.style="display:none";
-    usernameContainer.style="display:none";
-    profileContainer.style="display:flex";
+  mainIntro.style = "display:none";
+  menuContainer.style.display = "none";
+  messageField.style = "display:none";
+  usernameContainer.style = "display:none";
+  profileContainer.style = "display:flex";
 });
 
-
-const possibleMessages= {
-    clientMessage: [
-        "tell me about yourself",
-        "really?",
-        "im good",
-        "im good and you?",
-        "what are your hobbies?",
-        "when were you born?",
-        "what is your surname?",
-        "how are you?",
-        "what is your name?",
-        "show me your picture",
-        "haha",
-        "im doing good and you?",
-        "hey",
-        "hi",
-        "how are you doing?",
-        "how is your day going?",
-        "how old are you?",
-        "when were you created?",
-        "what is your purpose?",
-        "what langueges can you speak?"
-    ],
-    robotMessage: [
-        "I'm just a robot learning what humans ask me \n When i don't understand i will ask my coach to teach me how to answer them next time.",
-        "Yeah :)",
-        "perfect :) \n Ask me questions",
-        "Im good \n ask me questions dear.",
-        "I like dancing, and talking to humans when they are bored",
-        "I was born on the 8th of July 2020",
-        "They did not give me a surname... \n Tell me yours dear.",
-        "i am good and you?",
-        "My name is KatBot i am a robot",
-        "./Desert.jpg",
-        "Why are you laughing? :)",
-        "Good. \n Ask me any question",
-        "hi",
-        "hey",
-        "I'm doing good thanks and you?",
-        "it's going good thanks and you?",
-        "I am one day old",
-        "I was created today",
-        "I'm just here to relieve boredom",
-        "I was only taught english, but i will try to other langueges from you"
-    ]
+const possibleMessages = {
+  clientMessage: [
+    /tell me about yourself/i,
+    /really?/i,
+    /im good/i,
+    /i'm doing good and you?/i,
+    /what are your hobbies?/i,
+    /when were you born?/i,
+    /what is your surname?/i,
+    /how are you?/i,
+    /what is your name?/i,
+    /show me your picture/i,
+    /haha/i,
+    /im doing good and you?/i,
+    /hey/i,
+    /hi/i,
+    /how are you doing?/i,
+    /how is your day going?/i,
+    /how old are you?/i,
+    /when were you created?/i,
+    /what is your purpose?/i,
+    /what langueges can you speak?/i,
+  ],
+  robotMessage: [
+    "I'm just a robot learning what humans ask me \n When i don't understand i will ask my coach to teach me how to answer them next time.",
+    "Yeah :)",
+    "perfect :) \n Ask me questions",
+    "Im good \n ask me questions dear.",
+    "I like dancing, and talking to humans when they are bored",
+    "I was born on the 8th of July 2020",
+    "They did not give me a surname... \n Tell me yours dear.",
+    "i am good and you?",
+    "My name is KatBot i am a robot",
+    "./Desert.jpg",
+    "Why are you laughing? :)",
+    "Good. \n Ask me any question",
+    "hi",
+    "hey",
+    "I'm doing good thanks and you?",
+    "it's going good thanks and you?",
+    "I am one day old",
+    "I was created today",
+    "I'm just here to relieve boredom",
+    "I was only taught english, but i will try to other langueges from you",
+  ],
 };
 
-sendButton.addEventListener("click", (e) =>{
+sendButton.addEventListener("click", (e) => {
+  e.preventDefault();
 
-    e.preventDefault();
+  const liBibot = document.createElement("li");
+  const li = document.createElement("li");
 
-    const liBibot = document.createElement("li");
-    const li = document.createElement("li");
+  for (i = 0; i <= possibleMessages.clientMessage.length; i++) {
+    if (possibleMessages.clientMessage[i].test(input.value)) {
+      li.innerText = input.value;
+      messageList.append(li);
+      li.setAttribute("class", "me");
 
-    for (i = 0; i <= possibleMessages.clientMessage.length; i++){
-        
-        if(input.value == possibleMessages.clientMessage[i]){
+      liBibot.innerText = possibleMessages.robotMessage[i];
+      messageList.append(liBibot);
+      liBibot.setAttribute("class", "them");
 
-            li.innerText = input.value;
-            messageList.append(li);
-            li.setAttribute("class", "me");
+      input.value = "";
 
-            liBibot.innerText = possibleMessages.robotMessage[i]
-            messageList.append(liBibot);
-            liBibot.setAttribute("class", "them");
+      break;
+    } else {
+      li.innerText = input.value;
+      messageList.append(li);
+      li.setAttribute("class", "me");
 
-            input.value="";
-
-            break;
-        }else{
-
-            li.innerText = input.value;
-            messageList.append(li);
-            li.setAttribute("class", "me");
-
-            liBibot.innerText = "I do not understand what you are trying to say, please speak english atleast."
-            messageList.append(liBibot);
-            liBibot.setAttribute("class", "them");
-
-        }
-        
+      liBibot.innerText =
+        "I do not understand what you are trying to say, please speak english atleast.";
+      messageList.append(liBibot);
+      liBibot.setAttribute("class", "them");
     }
+  }
 
-    input.value="";
-    
- });
-
+  input.value = "";
+});
 
 //  Setting up the current time
 
@@ -265,7 +243,3 @@ const minute = document.getElementById("minute");
 
 hour.innerText = hours;
 minute.innerText = minutes;
-
-
-
- 
